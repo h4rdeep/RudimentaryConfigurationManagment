a84b95430cf6d9b7961a288c09abcb50d46994a9
Hardeep Singh <hardeep.singh.kapooria@gmail.com>
2021-07-30 16:11:27 +0530

    file module create and reducing function calls

--- rcm.py
+++ rcm.py
@@ -1,24 +1,37 @@
 #!/bin/python
 import json, sys
+import time
 import paramiko
 
-def command_over_ssh(ssh_args):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_port= ssh_args['port'] if 'port' in ssh_args else 22
-    try:
-        if 'pass' in ssh_args:
-            ssh.connect(ssh_args['ip'], ssh_port, ssh_args['username'], ssh_args['pass'])
-        elif 'ssh_key' in ssh_args:
-            ssh.connect(ssh_args['ip'], ssh_port, ssh_args['username'],  key_filename=ssh_args['ssh_key'])
-        stdin, stdout, stderr = ssh.exec_command(ssh_args['commands'],  get_pty=True)
-    except Exception as e:
-        exit(e)
-    ssh_result = stdout.readlines()
-    ssh_error = stderr.readlines()
-    # if ssh_error: 
-    #     print(ssh_error)
-    return ssh_result, ssh_error
+def command_over_ssh(ssh_args, mode="ssh"):
+        # time.sleep(2)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_port= ssh_args['port'] if 'port' in ssh_args else 22
+        try:
+            if 'pass' in ssh_args:
+                ssh.connect(ssh_args['ip'], ssh_port, ssh_args['username'], ssh_args['pass'])
+            elif 'ssh_key' in ssh_args:
+                ssh.connect(ssh_args['ip'], ssh_port, ssh_args['username'],  key_filename=ssh_args['ssh_key'])
+            if mode=="ssh":
+                stdin, stdout, stderr = ssh.exec_command(ssh_args['commands'],  get_pty=True)
+            elif mode=="scp":
+                ftp_client=ssh.open_sftp()
+                file_name=ssh_args["source_file"].split("/")[-1]
+                stdout = ftp_client.put(ssh_args["source_file"],"/tmp/"+file_name)
+                ftp_client.close()  
+        except Exception as e:
+            print("exception occured")
+            exit(e)
+        try:
+            ssh_result = stdout.readlines()
+            ssh_error = stderr.readlines()
+        except AttributeError as e:
+            ssh_result = stdout
+            ssh_error = []
+        if ssh_error:
+            print("SSH Error {}:".format(ssh_error))
+        return ssh_result, ssh_error
 
 def packages(package_info,ssh_info):
     def check_package():
@@ -31,23 +44,24 @@ def packages(package_info,ssh_info):
     # print(package_info)
     for pkg_block in package_info:
         for package in pkg_block['name']:
+            validate_package=check_package()
             if pkg_block['state'] == 'present':
                 print("+++++++++ \n Installing {}\n+++++++++".format(package))
-                if 'ok installed' in check_package()[0]:
+                if 'ok installed' in validate_package[0]:
                     print("'"+package + "' already installed. [Ok]")
                     break
-                elif 'no packages found' in check_package()[0]:
+                elif 'no packages found' in validate_package[0]:
                     ssh_info["commands"]="sudo apt-get -y install " + package
 
             elif pkg_block['state'] == 'absent':
                 print("+++++++++ \n Removing {}\n+++++++++".format(package))
-                if 'ok installed' in check_package()[0]:
+                if 'ok installed' in validate_package[0]:
                     ssh_info["commands"]="sudo apt-get -y --purge remove " + package
-                elif 'no packages found' in check_package()[0]:
+                elif 'no packages found' in validate_package[0]:
                     print("'"+package + "' not found. [Ok]")
                     break
                 else:
-                    exit(check_package())
+                    exit(validate_package)
             else:
                 exit("Packages state '{}' not supported.".format(pkg_block['state']))
             # print(pkg_block['state'])
@@ -60,8 +74,45 @@ def packages(package_info,ssh_info):
 
 def files(file_info, ssh_info):
     print("files function")
-    print(file_info)
-    print(ssh_info)
+    def check_files():
+        ssh_info['commands']="diff -qs /tmp/"+ ssh_info['source_file'].split('/')[-1] +" "+ssh_info['destination_file']
+        file_stts=command_over_ssh(ssh_info)
+        file_status= file_stts[0] if bool(command_over_ssh(ssh_info)[0]) else file_stts[1]
+        print(file_status)
+        return file_status
+    for file_block in file_info:
+        print("+++++++++ \n Moving Files: {}\n+++++++++".format(file_block["source"]))
+        ssh_info["source_file"]=file_block["source"]
+        ssh_info["destination_file"]=file_block["destination"]
+        ssh_info["file_mode"]=file_block["mode"]
+        ssh_info["file_owner"]=file_block["owner"]
+        ssh_info["file_group"]=file_block["group"] if 'group' in file_block else  file_block["owner"]
+        file_transfer=command_over_ssh(ssh_info,'scp')
+        print(file_transfer)
+        file_check=check_files()
+        if 'identical' in file_check[0]:
+            print("'"+file_block["source"] + "' and '"+ file_block["destination"] +"' are identical. [Ok]")
+            break
+        elif 'differ' in file_check[0] or 'No such file' in file_check[0]:
+            print("different")
+            print(ssh_info["source_file"].split("/")[-1])
+            if 'No such file' in file_check[0]:
+                ssh_info['commands']="sudo touch "+ ssh_info['destination_file']
+                # ssh_info['commands']="sudo cp /tmp/"+ ssh_info["source_file"].split("/")[-1] +" "+ ssh_info['destination_file']
+                create_file=command_over_ssh(ssh_info)
+                # print(create_file)
+            ssh_info['commands']="sudo chmod 777 "+ ssh_info['destination_file']
+            change_permission=command_over_ssh(ssh_info)
+            ssh_info['commands']="sudo cat /tmp/"+ ssh_info["source_file"].split("/")[-1] +" > "+ ssh_info['destination_file']
+            copy_content=command_over_ssh(ssh_info)
+            print("copy_content: {}".format(copy_content))
+            if "Permission denied" in copy_content:
+                exit(copy_content[0])
+            ssh_info["commands"]="sudo chown "+ ssh_info["file_owner"] + ":" + ssh_info["file_owner"]+" "+  ssh_info['destination_file'] +"; sudo chmod "+ ssh_info["file_mode"]+" "+ssh_info['destination_file']
+            file_perm=command_over_ssh(ssh_info)
+            print(file_perm)
+
+        
 
 def services(service_info, ssh_info):
     def check_service():
@@ -73,25 +124,26 @@ def services(service_info, ssh_info):
 
     for srv_block in service_info:
         for srvc in srv_block['name']:
+            validate_service=check_service()
             if srv_block['state'] == 'start':
                 print("+++++++++ \n Starting {} Service\n+++++++++".format(srvc))
-                if 'running' in check_service()[0]:
+                if 'running' in validate_service[0]:
                     print("'"+ srvc + "' already running. [Ok]")
                     break
-                elif 'dead' in check_service()[0]:
+                elif 'dead' in validate_service[0]:
                     ssh_info["commands"]="sudo systemctl start " + srvc
                 else:
-                    exit("Unrecognized service state '{}' for {} service.".format(check_service()[0],srvc))
+                    exit("Unrecognized service state '{}' for {} service.".format(validate_service[0],srvc))
 
             elif srv_block['state'] == 'stop':
                 print("+++++++++ \n Stopping {} Service\n+++++++++".format(srvc))
-                if 'running' in check_service()[0]:
+                if 'running' in validate_service[0]:
                     ssh_info["commands"]="sudo systemctl stop " + srvc
-                elif 'dead' in check_service()[0]:
+                elif 'dead' in validate_service[0]:
                     print("'"+ srvc + "' already in stopped condition. [Ok]")
                     break
                 else:
-                    exit("Unrecognized service state '{}' for {} service.".format(check_service()[0],srvc))
+                    exit("Unrecognized service state '{}' for {} service.".format(validate_service[0],srvc))
             elif srv_block['state'] == 'restart':
                 print("+++++++++ \n Restarting {} Service\n+++++++++".format(srvc))
                 ssh_info["commands"]="sudo systemctl restart " + srvc
@@ -107,7 +159,8 @@ def services(service_info, ssh_info):
                 print(line)
 def rudimentary_cm():
     ssh_args={}
-    play_file=sys.argv[1]
+    # play_file=sys.argv[1]
+    play_file = "playbook.json"
     playbook=open(play_file)
     play_data=json.load(playbook)
     for play in play_data:
